f4a4e4db6e03d820e5e571d4159217325717db59
Miguel Del Corso <migueldelcorso@gmail.com>
2025-07-15 11:59:36 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Miguel Del Corso on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
