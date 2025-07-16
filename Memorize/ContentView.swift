7360e969051384531c4dbb852cd11c92d809f132
Miguel Del Corso <migueldelcorso@gmail.com>
2025-07-16 10:08:17 -0300

    Tidy up just a little bit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,32 +1,61 @@
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View
+{
+    @State private var cardCount = 4
+    let emojis = ["👻", "🎃","💀", "🕷️", "👹", "😈"]
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+                ForEach(0..<cardCount, id: \.self) {
+                    CardView(content: emojis[$0])
+                }
+            }
+            Spacer()
+            HStack {
+                CardCountAdjuster(by: 1, symbol: "plus")
+                CardCountAdjuster(by: -1, symbol: "minus")
+            }
+            .padding()
         }
+        
+    }
+    
+    func CardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button {
+            cardCount += offset
+        } label: {
+            Image(systemName: symbol)
+        }
+        .buttonBorderShape(.circle)
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+
     }
 }
 
+
+
 struct CardView: View {
-    var isFaceUp: Bool = false
+    var content: String
+    @State var isFaceUp = Bool.random()
+    
     var body: some View {
         ZStack {
+            let rectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
-                Text("👻")
+                rectangle.fill(.background)
+                rectangle.strokeBorder(lineWidth: 2)
+                Text(content)
                     .font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
-            }
+                rectangle            }
         }
+        .frame(minHeight: 50)
         .foregroundStyle(.orange)
         .imageScale(.small)
-        .padding()
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
